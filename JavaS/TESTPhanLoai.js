464e926481172c1436c06c39bdbbd4e000bd7839
ThanhTung913 <thanhtungnguyen913@gmail.com>
2021-12-11 20:57:16 +0700

    Main

---
+++ JavaS/TESTPhanLoai.js
@@ -0,0 +1,21 @@
+/*let DTB = 7.6;
+if (DTB >= 9){
+    console.log('Xuat sac')
+} else {
+    if (DTB >= 8){
+        console.log('Gioi');
+    } else if(DTB >= 7){ 
+        console.log('Kha');
+    } else if (DTB >= 5){
+        console.log('TB');
+    } else {
+        console.log('yeu');
+    }
+}
+*/
+ let DTB =7.6;
+ if (DTB >9 ) {
+     console.log('Xuat Sac')
+if ( 9 > DTB && DTB >= 8){
+    console.log('Kha')
+}
